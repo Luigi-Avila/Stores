4aa763a100f2bf50c94f39b8bdc3af9e0aa0ef36
Luigi <l.avila.uvg@gmail.com>
2023-05-12 11:49:38 -0600

    feat: setup recyclerview function added

--- app/src/main/java/com/example/stores/MainActivity.kt
+++ app/src/main/java/com/example/stores/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.stores
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.stores.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var mBinding: ActivityMainBinding
+
+    private lateinit var mAdapter: StoreAdapter
+    private lateinit var mGridLayout: GridLayoutManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupRecyclerview()
+    }
+
+    private fun setupRecyclerview(){
+        mAdapter = StoreAdapter(mutableListOf(), this)
+        mGridLayout = GridLayoutManager(this, 2)
+
+        mBinding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = mGridLayout
+            adapter = mAdapter
+        }
+    }
+
+    override fun onclick(store: Store) {
+        super.onclick(store)
     }
 }
