ec241cd67ea1b77a0cbcb5c90941cd1be07562c7
Luigi-Avila <l.avila.uvg@gmail.com>
2023-05-15 11:21:38 -0600

    feat: dao interface added

---
+++ app/src/main/java/com/example/stores/StoreDao.kt
@@ -0,0 +1,23 @@
+package com.example.stores
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface StoreDao {
+
+    @Query("SELECT * FROM StoreEntity")
+    fun getAllStores(): MutableList<StoreEntity>
+
+    @Insert
+    fun insertStore(store: StoreEntity)
+
+    @Update
+    fun updateStore(store: StoreEntity)
+
+    @Delete
+    fun deleteStore(store: StoreEntity)
+}
