9ef66eceec812d2586a5b3eda009241eb79aa294
Luigi-Avila <l.avila.uvg@gmail.com>
2023-05-24 11:44:52 -0600

    feat: hide keyboard function and go home after saving a store

--- app/src/main/java/com/example/stores/EditStoreFragment.kt
+++ app/src/main/java/com/example/stores/EditStoreFragment.kt
@@ -1,5 +1,6 @@
 package com.example.stores
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import com.example.stores.databinding.FragmentEditStoreBinding
 import com.google.android.material.snackbar.Snackbar
 import java.util.concurrent.LinkedBlockingQueue
@@ -62,11 +64,13 @@ class EditStoreFragment : Fragment() {
                 }.start()
 
                 queue.take()?.let {
+                    hideKeyboard()
                     Snackbar.make(
                         mBinding.root,
                         getString(R.string.edit_store_message_save_success),
                         Snackbar.LENGTH_SHORT
                     ).show()
+                    mActivity?.onBackPressedDispatcher?.onBackPressed()
                 }
                 true
             }
@@ -75,6 +79,15 @@ class EditStoreFragment : Fragment() {
         }
     }
 
+    private fun hideKeyboard(){
+        val imm = mActivity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+    }
+
+    override fun onDestroyView() {
+        hideKeyboard()
+        super.onDestroyView()
+    }
     override fun onDestroy() {
         mActivity?.supportActionBar?.setDisplayHomeAsUpEnabled(false)
         mActivity?.supportActionBar?.title = getString(R.string.app_name)
