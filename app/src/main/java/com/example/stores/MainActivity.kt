ee82261109bb775f2c349c98b8ed0dc79cd583e3
Luigi-Avila <l.avila.uvg@gmail.com>
2023-05-31 12:05:38 -0600

    feat: change the number of columns when the phone is in landscape mode and add padding with clipToPadding

--- app/src/main/java/com/example/stores/MainActivity.kt
+++ app/src/main/java/com/example/stores/MainActivity.kt
@@ -52,7 +52,7 @@ class MainActivity : AppCompatActivity(), OnClickListener, MainAux {
 
     private fun setupRecyclerview() {
         mAdapter = StoreAdapter(mutableListOf(), this)
-        mGridLayout = GridLayoutManager(this, 2)
+        mGridLayout = GridLayoutManager(this, resources.getInteger(R.integer.main_columns))
 
         getStores()
 
